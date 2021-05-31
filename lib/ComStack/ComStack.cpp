349b56b4d7dad8f5987527594c41d4286f12cc0c
Augustin.B <augustinbielefeld@gmail.com>
2021-05-31 19:52:04 +0200

    ComStack: Added blank source files and header

---
+++ lib/ComStack/ComStack.cpp
@@ -0,0 +1,6 @@
+/*
+ * ComStack.cpp
+ *
+ *  Created on: 31.05.2021
+ *      Author: AB
+ */
---
+++ lib/ComStack/ComStack.hpp
@@ -0,0 +1,11 @@
+/*
+ * ComStack.hpp
+ *
+ *  Created on: 31.05.2021
+ *      Author: AB
+ */
+
+#ifndef COMSTACK_HPP_
+#define COMSTACK_HPP_
+
+#endif /* COMSTACK_HPP_ */
